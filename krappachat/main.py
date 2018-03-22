bde3c6380b066015d02cd58588f0b1288d2ba1fd
Chronophylos <Chronophylos@users.noreply.github.com>
2018-03-22 21:00:11 +0100

    Squashed commit of the following:
    commit 1233d54b034ca8479e213c80951fb350c094612b
    Author: nvalis <nvalis@users.noreply.github.com>
    Date:   Thu Mar 22 20:48:18 2018 +0100
    
        Add the ability to send messages and fix docstrings
    
    commit c2cb622d6ca05c9cf91976d43c97b0434e099b31
    Author: nvalis <nvalis@users.noreply.github.com>
    Date:   Tue Mar 20 07:07:59 2018 +0100
    
        Add timestamps and user colors
    
    commit ac4cafa1b3cd11fb68de4ef3a8fa57a7b79432b6
    Author: nvalis <nvalis@users.noreply.github.com>
    Date:   Tue Mar 20 03:30:40 2018 +0100
    
        Add crude chat interface

--- krappachat/main.py
+++ krappachat/main.py
@@ -1,18 +1,42 @@
 """Main module of KrappaChat containing the KrappaChatApp kivy application."""
 
 import logging
-import pickle
 import sys
+import pickle
+import threading
 
 from kivy.app import App
 from kivy.utils import platform
-from pythonosc import osc_server, dispatcher
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from pythonosc import osc_server, udp_client, dispatcher
+logging.basicConfig(level=logging.INFO)
 
-logging.basicConfig(level=logging.DEBUG)
+
+class ChatView(BoxLayout):
+	"""Kivy chat view widget."""
+
+	def __init__(self, osc_client, **kwargs):
+		"""Initialize a new ChatView with the given OSC client to forward messages."""
+		super().__init__(**kwargs)
+		self.osc_client = osc_client
+		self.cols = 1
+
+	def send_message(self, target, message):
+		"""Forward message to send to the OSC server."""
+		self.osc_client.send_message('/send_message', [target, message])
+
+	def add_event(self, event):
+		"""Add a new message event to the RecycleView."""
+		self.rv.data.append({'event': event})
+
+	def clear(self):
+		"""Clear all message Rows from the RecycleView."""
+		self.rv.data = []
 
 
 class KrappaChatApp(App):
-	"""Main kivy application responsible for GUI and background service	handling."""
+	"""Main kivy application responsible for GUI and background service handling."""
 
 	def build(self):
 		"""Build main application, initialize background service and OSC server."""
@@ -21,7 +45,6 @@ class KrappaChatApp(App):
 			from android import AndroidService
 			self.service = AndroidService(title='IRCService')
 		elif platform in ['linux', 'win']:
-			import threading
 			from krappachat.service.main import main
 			self.service = threading.Thread(target=main, args=())
 			self.service.daemon = True
@@ -29,26 +52,34 @@ class KrappaChatApp(App):
 			logging.critical(f'Currently not supported platform!')
 			sys.exit(1)
 		self.service.start()
+
+		# OSC server
 		dp = dispatcher.Dispatcher()
-		dp.map('/pubmsg', self.on_pubmsg)
-		dp.map('/whisper', self.on_whisper)
+		dp.map('/pubmsg', self.handle_pubmsg)
+		dp.map('/whisper', self.handle_whisper)
 		self.osc_server = osc_server.ThreadingOSCUDPServer(('127.0.0.1', 3000), dp)
 		server_thread = threading.Thread(target=self.osc_server.serve_forever)
 		server_thread.start()
+
+		# OSC client
+		self.osc_client = udp_client.SimpleUDPClient('127.0.0.1', 3001)
+
+		self.chat_view = ChatView(self.osc_client)
+		return self.chat_view
 
 	def on_stop(self):
 		"""On application stop shut down the OSC server."""
 		self.osc_server.shutdown()
 
-	def on_pubmsg(self, message, event):
+	def handle_pubmsg(self, message, event):
 		"""Event method handling public channel messages."""
 		event = pickle.loads(event)
-		self.root.text += f'Pubmsg: {event.arguments[0]}\n'
+		self.chat_view.add_event(event)
 
-	def on_whisper(self, message, event):
+	def handle_whisper(self, message, event):
 		"""Event method handling private whispers."""
 		event = pickle.loads(event)
-		self.root.text += f'Whisper: {event.arguments[0]}\n'
+		self.chat_view.add_event(event)
 
 
 if __name__ == '__main__':
--- krappachat/service/main.py
+++ krappachat/service/main.py
@@ -5,15 +5,49 @@ background service to run a TwitchChatClient instance.
 """
 
 import logging
+import sys
 import pickle
 import ssl
-import sys
+import datetime
+import threading
 
 import irc.client
 import irc.connection
-from pythonosc import udp_client
+from pythonosc import osc_server, udp_client, dispatcher
 
 logging.basicConfig(level=logging.DEBUG)
+
+
+class ChatEvent:
+	"""Data storage class for all chat events."""
+
+	def __init__(self, event):
+		"""Create a new ChatEvent from a given event of type irc.client.Event."""
+		self.source = event.source
+		self.target = event.target
+		self.type = event.type
+		self.message = ' '.join(event.arguments)
+		tags = {i['key']: i['value'] for i in event.tags}
+		self.timestamp_ms = int(tags['tmi-sent-ts'])
+		self.time_string = self._get_sent_time_string()
+		self.badges = tags['badges'].split(',') if tags['badges'] else []
+		self.user_name = tags['display-name']
+		self.user_color = tags['color'] if tags['color'] else self._get_default_color_for_user(self.user_name)
+		self.user_is_mod = tags['mod'] == '1'
+		self.user_is_sub = tags['subscriber'] == '1'
+		self.user_is_turbo = tags['turbo'] == '1'
+		# Missing: self.user_type, self.user_id, self.room_id, self.emotes, self.id
+
+	def _get_sent_time_string(self, format='%H:%M:%S'):
+		"""Return the sent time in readable form in the given format."""
+		dt = datetime.datetime.fromtimestamp(self.timestamp_ms / 1000)
+		return dt.strftime(format)
+
+	def _get_default_color_for_user(self, name):
+		"""Following: https://discuss.dev.twitch.tv/t/default-user-color-in-chat/385/2 ."""
+		n = ord(name[0]) + ord(name[-1])
+		default_colors = ['#FF0000', '#0000FF', '#00FF00', '#B22222', '#FF7F50', '#9ACD32', '#FF4500', '#2E8B57', '#DAA520', '#D2691E', '#5F9EA0', '#1E90FF', '#FF69B4', '#8A2BE2', '#00FF7F']
+		return default_colors[n % len(default_colors)]
 
 
 class TwitchChatClient(irc.client.SimpleIRCClient):
@@ -31,8 +65,19 @@ class TwitchChatClient(irc.client.SimpleIRCClient):
 		"""
 		server, port = 'irc.chat.twitch.tv', 443
 		self.channels = channels
+		super().__init__()
+
+		# OSC client
 		self.osc_client = udp_client.SimpleUDPClient('127.0.0.1', 3000)
-		irc.client.SimpleIRCClient.__init__(self)
+
+		# OSC server
+		dp = dispatcher.Dispatcher()
+		dp.map('/send_message', self.send_message)
+		self.osc_server = osc_server.ThreadingOSCUDPServer(('127.0.0.1', 3001), dp)
+		server_thread = threading.Thread(target=self.osc_server.serve_forever)
+		server_thread.start()
+
+		# IRC connection
 		connect_factory = irc.connection.Factory(wrapper=ssl.wrap_socket)
 		self.connect(server, port, nickname, password=oauth_token,
 					 connect_factory=connect_factory)
@@ -60,11 +105,15 @@ class TwitchChatClient(irc.client.SimpleIRCClient):
 
 	def on_pubmsg(self, connection, event):
 		"""Public message handling forwarding the event via OSC."""
-		self.osc_client.send_message('/pubmsg', pickle.dumps(event))
+		self.osc_client.send_message('/pubmsg', pickle.dumps(ChatEvent(event)))
 
 	def on_whisper(self, connection, event):
 		"""Whisper message handling forwarding the event via OSC."""
-		self.osc_client.send_message('/whisper', pickle.dumps(event))
+		self.osc_client.send_message('/whisper', pickle.dumps(ChatEvent(event)))
+
+	def send_message(self, command, target, message):
+		"""Send message to IRC."""
+		self.connection.privmsg(target, message)
 
 
 def main():
