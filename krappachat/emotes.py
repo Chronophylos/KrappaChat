c73024a90d3f75eb33972e73f2c9af17f2dde6a5
Chronophylos <Chronophylos@users.noreply.github.com>
2018-04-01 17:25:03 +0200

    add caching to emotes
    Signed-off-by: Chronophylos <Chronophylos@users.noreply.github.com>

--- krappachat/emotes.py
+++ krappachat/emotes.py
@@ -1,5 +1,6 @@
 """Module for emote handling."""
 
+import os
 from enum import Enum
 from io import BytesIO
 
@@ -7,7 +8,7 @@ import requests
 from PIL import Image
 
 
-class EmoteType(Enum):
+class EmoteTypes(Enum):
 	"""Enum class to specify emote type."""
 
 	Twitch = 0,
@@ -15,7 +16,7 @@ class EmoteType(Enum):
 	BetterTwitchTV = 2,
 
 
-class EmoteResolution(Enum):
+class EmoteResolutions(Enum):
 	"""Enum class to specify emote resolution."""
 
 	Small = 1
@@ -23,45 +24,85 @@ class EmoteResolution(Enum):
 	Big = 3
 
 
+class EmoteCache:
+	"""Class for caching emotes."""
+
+	location = 'cache'
+	index = dict()
+
+	@staticmethod
+	def load(name: str):
+		"""Load a file from cache.
+
+		Load a file from cache if it exists otherwise raise
+		FileNotFoundError.
+
+		:param name: a string to search for in the cache
+		:return: a byte-like object
+		"""
+		if name not in EmoteCache.index:
+			path = os.path.join(EmoteCache.location, name)
+			if os.path.exists(path):
+				EmoteCache.index[name] = path
+			else:
+				raise FileNotFoundError
+		with open(EmoteCache.index[name], 'rb') as cached_file:
+			data = cached_file.read()
+		return data
+
+	@staticmethod
+	def cache(name: str, data: BytesIO):
+		"""Cache a byte-like object.
+
+		:param name: a name for the cached file
+		:param data: a byte-like object to cache
+		"""
+		path = os.path.join(EmoteCache.location, name)
+		with open(path, 'wb') as cached_file:
+			cached_file.write(data.getvalue())
+		EmoteCache.index[name] = path
+
+
 class Emote:
 	"""Class to download and store an emote."""
 
 	images = dict()
 
-	def __init__(self, name: str, url: str, emote_type: EmoteType):
+	def __init__(self, name: str, url: str, emote_type: EmoteTypes):
 		"""Create new emote by name and url."""
 		self.name = name
 		self.url = url
 		if not url.endswith('/'):
 			self.url += '/'
 		self.emote_type = emote_type
+		# TODO: make async
+		for res in EmoteResolutions:
+			self.images[res] = EmoteCache.load(self.name + res)
 
-	# TODO: load cached images
-
-	def download(self):
+	def download(self, res: EmoteResolutions):
 		"""Download emote image data."""
-		# TODO: cache images
-		for res in EmoteResolution:
-			suffix = Emote.get_suffix_for_type(res, self.emote_type)
-			r = requests.get(self.url + suffix)
-			if not r.status_code == 200:
-				raise Exception
-			self.images[res] = Image.open(BytesIO(r.content))
+		suffix = Emote.get_suffix_for_type(res, self.emote_type)
+		r = requests.get(self.url + suffix)
+		if not r.status_code == 200:
+			raise Exception
+		b = BytesIO(r.content)
+		EmoteCache.cache(self.name + res.value, b)
+		self.images[res] = Image.open(b)
 
 	@staticmethod
-	def get_suffix_for_type(res: EmoteResolution, emote_type: EmoteType):
+	def get_suffix_for_type(res: EmoteResolutions, emote_type: EmoteTypes):
 		"""Get URL suffix for given emote resolution and type."""
-		if emote_type == EmoteType.Twitch:
+		if emote_type == EmoteTypes.Twitch:
 			return str(res.value) + '.0'
-		if emote_type == EmoteType.FrankerFaceZ:
+		if emote_type == EmoteTypes.FrankerFaceZ:
 			value = 4 if res.value == 3 else res.value
 			return str(value)
-		if emote_type == EmoteType.BetterTwitchTV:
+		if emote_type == EmoteTypes.BetterTwitchTV:
 			return str(res.value) + 'x'
 
 
 class EmoteList:
-	"""Stores all cached emotes."""
+	"""Stores all emotes."""
 
 	def __init__(self):
 		"""Create new list of emotes."""
